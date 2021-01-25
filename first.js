78bd1e45a4ae1ab13308d9b03429fd668fb639ff
Lokesh Singh <lksrajawat@gmail.com>
2021-01-25 23:37:09 +0530

    color theme

--- first.js
+++ first.js
@@ -32,3 +32,24 @@ $(document).ready(function(){
     });
   });
 })
+
+document.body.onload = ()=>{
+  
+    const redBtn = document.querySelector('#toggle-red');
+    const blueBtn = document.querySelector('#toggle-blue');
+    const greenBtn = document.querySelector('#toggle-green');
+    
+    redBtn.addEventListener('click',(e)=>{
+   
+      console.log("red");
+      document.documentElement.style.setProperty('--main-hue', 14);
+   })
+    
+  blueBtn.addEventListener('click',(e)=>{
+      document.documentElement.style.setProperty('--main-hue', 240);
+    })
+    
+    greenBtn.addEventListener('click',(e)=>{
+      document.documentElement.style.setProperty('--main-hue', 120);
+    })  
+  }
